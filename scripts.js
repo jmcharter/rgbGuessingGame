2ef5f3a3ba0faa63ff7304c3fc78d710d7f05922
Jon Charter <jmcharter@gmail.com>
2018-04-12 00:18:17 +0100

    logic added for correct guess, changes all square colours

--- scripts.js
+++ scripts.js
@@ -8,7 +8,37 @@ var colours = [
 ];
 
 var squares = document.querySelectorAll(".square");
+var colourDisplay = document.querySelector("#colourDisplay");
+var pickedColour = colours[4].toUpperCase();
+var infoDisplay = document.querySelector("#infoDisplay");
 
+colourDisplay.textContent = pickedColour;
+
+// Iterate through squares.
 for (i = 0; i < squares.length; i++){
+
+    // Add colours to to squares.
     squares[i].style.backgroundColor = colours[i];
+
+    // Add click listeners to squares.
+    squares[i].addEventListener("click",function(){
+        var clickedColour = this.style.backgroundColor;
+
+        // Check if correct square is clicked.
+        if(clickedColour === pickedColour.toLowerCase()){
+            infoDisplay.textContent = "Correct!";
+            colourChange(pickedColour);
+        } else {
+            this.style.backgroundColor = "#242424";
+            infoDisplay.textContent = "Try again.";
+        }
+    });
+}
+
+function colourChange(colour){
+
+    for (i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colour;
+    }
+
 }
